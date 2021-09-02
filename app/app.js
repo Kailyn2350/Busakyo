9675c137d13e6cc70e6d03677060631ff19d9db3
Kailyn2359 <seoks760577@naver.com>
2021-09-02 08:22:59 +0000

    make Js for front

--- app/app.js
+++ app/app.js
@@ -11,7 +11,7 @@ const home = require("./src/routes/home");
 //앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
-
+app.use(express.static(`${__dirname}/src/public`)); //__dirname = app.js를 반환 해준다. 그 위에있는 src에 있는 public폴더를 static = 정적 경로로 추가해주겠다 추가로 : `${~~~~}`여기 부분 `이거랑 '이거랑 구분 잘하기.
 
 app.use("/", home); //use -> 미들 웨어를 등록해주는 메서드. 일단 외우도록!
 
