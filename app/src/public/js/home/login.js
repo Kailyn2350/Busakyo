874345b3b063cc3e5fe7592f64ae6db9dda5b337
Kailyn2359 <seoks760577@naver.com>
2021-09-04 01:49:43 +0000

    Create a fetch requesting login

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -14,6 +14,12 @@ function login() {
 		id : id.value, //id.value에 접근하면 해당 id의 값에 접근할수 있다.
 		psword : psword.value,
 	}; //req라는 변수에 담아두는 거임 아이디랑 패스워드를
-	console.log(req);
-	//fetch(); //id랑 pw의 해당하는 데이터를 서버에 전달하는거
+	
+	fetch("/login", { //서버에서 받을려면 /login이라는 경로
+		method : "POST", //POST라는 메소드로 받을수 있는 API가 마련이 되있어야함 그런데 아직은 API는 마련 안하고 경로만 만들어졌다고 치고 입력해두는거
+		headers : {
+			"Content-Type" : "application/json", //내가 보내는 데이터의 타입을 명시하는 것이 Content-Type이라는 Key임 application/json 이러한 형태로  JSON을 전달을 한다
+		},
+		body : JSON.stringify(req) // body 라는 Key값으로 req를 전달 하는데 JSON이라는 타입으로 보내줄건데 JSON으로 감싸주고  stringify 메소드를 이용해서 req object를 JSON으로 감싸주는거임 stringify는 object를 단순히 문자열로 바꿔주는거
+	}); //id랑 pw의 해당하는 데이터를 서버에 전달하는거 ()안에 있는거는 경로 어떤 경로로 아이디랑 비밀번호를 보내줄 건지
 }
