bb69af6640d39e84f84874713cc54b5a0fb66440
Kailyn2359 <seoks760577@naver.com>
2021-09-05 01:10:00 +0000

    로그인 인증 기능 만들기  in 서버

--- ncoa/src/public/js/home/login.js
+++ ncoa/src/public/js/home/login.js
@@ -1,26 +1,41 @@
 "use strict";
-
+//login.js 프론트 엔드가 만들 화면 자바스크립트
 //DOM -> Document Object Model
 //Document 객체 모델 한마디로 영어 그 자체 일종의 인터페이스 이것을 사용해서 자바 스크립티에서 HTML에 존재하는 데이터를 가져와서 제어한다
 
-const id = document.querySelector("#id"), //querySelector는 질의 선택자 선택자를 통해서 HTML의 값을 가져올수 있다 #은 태그에 id로 부여되어 있는 id를 가져와 달라는 의미
+const id = document.querySelector("#id"), 
+//querySelector는 질의 선택자 선택자를 통해서 HTML의 값을 가져올수 있다 #은 태그에 id로 부여되어 있는 id를 가져와 달라는 의미
 	  psword = document.querySelector("#psword"), 
-	  loginBtn = document.querySelector("button"); //버튼 버튼은 하나여서 굳이 선택자 안하고 버튼 태그 그대로 쓰기
+	  loginBtn = document.querySelector("button"); 
+//버튼 버튼은 하나여서 굳이 선택자 안하고 버튼 태그 그대로 쓰기
 
 loginBtn.addEventListener("click", login);
 
 function login() {
 	const req = {
-		id : id.value, //id.value에 접근하면 해당 id의 값에 접근할수 있다.
+		id : id.value, 
+//id.value에 접근하면 해당 id의 값에 접근할수 있다.
 		psword : psword.value,
-	}; //req라는 변수에 담아두는 거임 아이디랑 패스워드를
+	}; 
+//req라는 변수에 담아두는 거임 아이디랑 패스워드를
 	
-	fetch("/login", { //서버에서 받을려면 /login이라는 경로
-		method : "POST", //POST라는 메소드로 받을수 있는 API가 마련이 되있어야함 그런데 아직은 API는 마련 안하고 경로만 만들어졌다고 치고 입력해두는거
+	fetch("/login", { 
+//서버에서 받을려면 /login이라는 경로
+		method : "POST", 
+//POST라는 메소드로 받을수 있는 API가 마련이 되있어야함 그런데 아직은 API는 마련 안하고 경로만 만들어졌다고 치고 입력해두는거
 		headers : {
-			"Content-Type" : "application/json", //내가 보내는 데이터의 타입을 명시하는 것이 Content-Type이라는 Key임 application/json 이러한 형태로  JSON을 전달을 한다
+			"Content-Type" : "application/json", 
+//내가 보내는 데이터의 타입을 명시하는 것이 Content-Type이라는 Key임 application/json 이러한 형태로  JSON을 전달을 한다
 		},
-		body : JSON.stringify(req) // body 라는 Key값으로 req를 전달 하는데 JSON이라는 타입으로 보내줄건데 JSON으로 감싸주고  stringify 메소드를 이용해서 req object를 JSON으로 감싸주는거임 stringify는 object를 단순히 문자열로 바꿔주는거 
+		body : JSON.stringify(req) 
+// body 라는 Key값으로 req를 전달 하는데 JSON이라는 타입으로 보내줄건데 JSON으로 감싸주고  stringify 메소드를 이용해서 req object를 JSON으로 감싸주는거임 stringify는 object를 단순히 문자열로 바꿔주는거 
 //프론트 엔드에서 body로 req데이터를 전달을 했으며 home.ctrl.js에서 process객체에서 받음
-	}); //id랑 pw의 해당하는 데이터를 서버에 전달하는거 ()안에 있는거는 경로 어떤 경로로 아이디랑 비밀번호를 보내줄 건지
+	}).then((res) => res.json()) 
+//반환값을 전달을 해줘야함
+	  .then(console.log);
+//id랑 pw의 해당하는 데이터를 서버에 전달하는거 ()안에 있는거는 경로 어떤 경로로 아이디랑 비밀번호를 보내줄 건지
+//fetch에서 전달을 해서 다시 서버에서 받을려면 fetch끝에 then이라는 메소드르 붙여서 res라는 데이터가 => (파라미터로 전달 되고)
+//res.json()의 반환 값은 promise임 기본 res의 반환 값은 response스트림(뜻: 데이터를 사용자에게 지속적으로 적은 양의)인데, ".json()"메서드를 통해 response스트림을 읽을수 있다. ".json()"으로 response스트림을 가져와 완료될 때까지 읽는다. 다읽은 body의 텍스트를 promise 형태로 반환한다.
+//promise형태로 받는데 이를 받기 위해서는 뒤에 then을 한번더 찍어주고 res를 받음
+
 }
--- ncoa/src/routes/home/home.ctrl.js
+++ ncoa/src/routes/home/home.ctrl.js
@@ -1,5 +1,6 @@
 "use strict";
 
+
 const output = {
 	home : (req, res) => { //home함수는 그냥 해당 페이지("home/index"를 render 한마디로 렌더링 해주는 함수 -> output 이라는 객체로 빼주기 위해서 home이라는 Key값으로 이용하고
 		res.render("home/index");
@@ -8,15 +9,36 @@ const output = {
 	login : (req, res) => { //login함수는 그냥 해당 페이지("home/index"를 render 한마디로 렌더링 해주는 함수 
 		res.render("home/login");
 	},
-}
+};
+
+const users = {
+	id : ["apc7072", "seoks760577", "Kailyn235000"],
+	psword : ["seo890624@", "seokyu0102@", "seo890624"],
+};
 
 const process = {
 	login : (req, res) =>{
-		console.log(req.body); //login.js에서 body에 접급할려면 req(요청)을 하고 body에 접근을 한다 body데이터를 받을려면 모듈을 설치 해줘야됨 
-	}
-}
+		const id = req.body.id,
+			  psword = req.body.psword;//login.js에서 body에 접급할려면 req(요청)을 하고 body에 접근을 한다 body데이터를 받을려면 모듈을 설치 해줘야됨 
+	
+		if (users.id.includes(id)){
+			const idx = users.id.indexOf(id);
+			if (users.psword[idx] === psword){ //users안에 있는 psword의 idx가 프론트엔드에서 전달한 psword가 같은지 여부 확인
+				return res.json({ //프론트 엔드한테 로그인이 성공했는지 여부를 응답해주는거임 똑같은 json 형태
+						success : true,//로그인이 성공하게 되면 success true 라는 objcet를 json으로 만들어서 res으로 프론트로 보내는거임
+				});
+			}
+		}
+		
+		return res.json({ //위서 return으로 나가져버리기 때문에 else구문은 안써도 됨
+			success : false,
+			msg : "로그인에 실패하셨습니다.",
+		})
+	},
+};
+//프론트 엔드에서 전달한 includes(id)의 id가 users 안에 id에 있으면 id의 idx를 가져옴 
 
 module.exports = {
 	output,
 	process,
-} //module exports로 밖으로 내보내는거임 받는거는 index.js에 컨트롤러 부분에서
+}; //module exports로 밖으로 내보내는거임 받는거는 index.js에 컨트롤러 부분에서
